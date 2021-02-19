452c3e6931c29b8f82953b67e3b40795c7544f56
Mark Wilkins <contactme@markwilkins.co>
2021-02-19 16:44:59 -0500

    updates resume

--- src/containers/Resume/Resume.js
+++ src/containers/Resume/Resume.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import styled from 'styled-components';
-import Logo from '../../elements/Logo';
-import SocialIcon from '../../elements/SocialIcon';
-import Content from '../../elements/Content';
-import GreenText from '../../elements/GreenText';
-import { StaticQuery, graphql } from 'gatsby';
-import ResumeInfoBlock from '../../components/ResumeInfoBlock';
-import dayjs from 'dayjs';
-import './Resume.css';
+import React from "react";
+import styled from "styled-components";
+import Logo from "../../elements/Logo";
+import SocialIcon from "../../elements/SocialIcon";
+import Content from "../../elements/Content";
+import GreenText from "../../elements/GreenText";
+import { StaticQuery, graphql } from "gatsby";
+import ResumeInfoBlock from "../../components/ResumeInfoBlock";
+import dayjs from "dayjs";
+import "./Resume.css";
 
 const ResumeTitle = styled.h1`
-    color: ${props => props.theme.black};
-    font-size: 4.2rem;
-    font-family: ${props => props.theme.headerFont};
-    text-transform: capitalize;
-    font-weight: bold;
-    padding: 35px 0;
+  color: ${(props) => props.theme.black};
+  font-size: 4.2rem;
+  font-family: ${(props) => props.theme.headerFont};
+  text-transform: capitalize;
+  font-weight: bold;
+  padding: 35px 0;
 `;
 
 const Main = styled.main`
@@ -25,40 +25,33 @@ const Main = styled.main`
 
 const ResumeSectionHeading = styled.h2`
   font-size: 1.6rem;
-  font-family: ${props => props.theme.headerFont};
+  font-family: ${(props) => props.theme.headerFont};
   font-weight: bold;
   padding: 15px 0;
-  color: ${props => props.theme.green};
+  color: ${(props) => props.theme.green};
 `;
 
 const InfoBlockLi = styled.li`
   margin: 10px 0;
+  padding-top: 5px;
+  break-inside: avoid;
 `;
 
-const presentFirst = ({node:a},{node:b}) => {
-  if (
-      a.end.toLowerCase() === 'present' &&
-      b.end.toLowerCase() !== 'present'
-    ) {
-      return -1;
-    }
+const presentFirst = ({ node: a }, { node: b }) => {
+  if (a.end.toLowerCase() === "present" && b.end.toLowerCase() !== "present") {
+    return -1;
+  }
 
-    if (
-      a.end.toLowerCase() !== 'present' &&
-      b.end.toLowerCase() === 'present'
-    ) {
-      return 1;
-    }
+  if (a.end.toLowerCase() !== "present" && b.end.toLowerCase() === "present") {
+    return 1;
+  }
 
-    if (
-      a.end.toLowerCase() === 'present' &&
-      b.end.toLowerCase() === 'present'
-    ) {
-      return 0;
-    }
+  if (a.end.toLowerCase() === "present" && b.end.toLowerCase() === "present") {
+    return 0;
+  }
 
-    return dayjs(b.end).valueOf() - dayjs(a.end).valueOf();
-}
+  return dayjs(b.end).valueOf() - dayjs(a.end).valueOf();
+};
 
 const Resume = () => (
   <StaticQuery
@@ -89,32 +82,32 @@ const Resume = () => (
         }
       }
     `}
-    render={data => {
+    render={(data) => {
       const workExperienceArr = data.allWorkHistoryJson.edges;
       const educationExperienceArr = data.allEducationJson.edges;
       return (
-      <Main>
-        <ResumeTitle>
-          <GreenText>Mark</GreenText> Wilkins
-        </ResumeTitle>
-        <ResumeSectionHeading>Experience</ResumeSectionHeading>
-        <ul>
-          {workExperienceArr.sort(presentFirst).map(({node}) => (
+        <Main>
+          <ResumeTitle>
+            <GreenText>Mark</GreenText> Wilkins
+          </ResumeTitle>
+          <ResumeSectionHeading>Experience</ResumeSectionHeading>
+          <ul>
+            {workExperienceArr.sort(presentFirst).map(({ node }) => (
               <InfoBlockLi key={node.id}>
-                <ResumeInfoBlock data={node}/>
+                <ResumeInfoBlock data={node} />
               </InfoBlockLi>
             ))}
-        </ul>
-        <ResumeSectionHeading>Education</ResumeSectionHeading>
-        <ul>
-          {educationExperienceArr.map(({node}) => (
+          </ul>
+          <ResumeSectionHeading>Education</ResumeSectionHeading>
+          <ul>
+            {educationExperienceArr.map(({ node }) => (
               <InfoBlockLi key={node.id}>
-                <ResumeInfoBlock data={node}/>
+                <ResumeInfoBlock data={node} />
               </InfoBlockLi>
             ))}
-        </ul>
-      </Main>
-    )
+          </ul>
+        </Main>
+      );
     }}
   />
 );
--- src/containers/ResumeSideBar.js
+++ src/containers/ResumeSideBar.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import styled from 'styled-components';
-import Logo from '../elements/Logo';
-import SocialIcon from '../elements/SocialIcon';
-import Content from '../elements/Content';
+import React from "react";
+import styled from "styled-components";
+import Logo from "../elements/Logo";
+import SocialIcon from "../elements/SocialIcon";
+import Content from "../elements/Content";
 
 const SideBar = styled.aside`
   width: 33.333%;
-  background: ${props => props.theme.resume.grey};
+  background: ${(props) => props.theme.resume.grey};
   padding: 30px;
   @media screen and (max-width: 775px) {
     width: 100%;
@@ -14,7 +14,7 @@ const SideBar = styled.aside`
 `;
 
 const SideBarHeading = styled.h3`
-  font-family: ${props => props.theme.headerFont};
+  font-family: ${(props) => props.theme.headerFont};
   color: white;
   font-size: 1.8rem;
   letter-spacing: 1px;
@@ -23,12 +23,12 @@ const SideBarHeading = styled.h3`
 const SocialText = styled.p`
   color: white;
   font-size: 1.6rem;
-  font-family: ${props => props.theme.contentFont};
+  font-family: ${(props) => props.theme.contentFont};
   max-width: 100%;
   word-break: break-all;
   @media all and (max-width: 900px) {
-      font-size: 1rem;
-    }
+    font-size: 1rem;
+  }
 `;
 
 const SocialLit = styled.ul`
@@ -54,41 +54,59 @@ const JobTitle = styled(SideBarHeading)`
 `;
 
 const SidebarParagraph = styled.p`
-    @media all and (max-width: 900px) {
-      font-size: 1rem;
-    }
+  @media all and (max-width: 900px) {
+    font-size: 1rem;
+  }
 `;
 
 const ResumeSideBar = () => (
   <SideBar>
     <Logo size="150px" />
-    <JobTitle>Full-Stack Developer</JobTitle>
+    <JobTitle>Software Engineer</JobTitle>
     <SocialLit>
+      {/* <SocialListItem>
+        <SocialIcon
+          color="white"
+          background={(props) => props.theme.green}
+          icon="home"
+        />
+        <SocialText> https://markwilkins.co</SocialText>
+      </SocialListItem> */}
       <SocialListItem>
-        <SocialIcon color="white" background={props => props.theme.green} icon="home"/><SocialText> https://markwilkins.co</SocialText>
+        <SocialIcon
+          color="white"
+          background={(props) => props.theme.green}
+          icon="phone"
+        />
+        <SocialText> (647) - 402 - 5501</SocialText>
       </SocialListItem>
       <SocialListItem>
-        <SocialIcon color="white" background={props => props.theme.green} icon="phone"/><SocialText> (647) - 402 - 5501</SocialText>
+        <SocialIcon
+          color="white"
+          background={(props) => props.theme.green}
+          icon="envelope"
+        />
+        <SocialText> contactme@markwilkins.dev</SocialText>
       </SocialListItem>
       <SocialListItem>
-        <SocialIcon color="white" background={props => props.theme.green} icon="envelope" /><SocialText> contactme@markwilkins.co</SocialText>
-      </SocialListItem>
-      <SocialListItem>
-        <SocialIcon color="white" background={props => props.theme.green} icon="linkedin" /><SocialText> /markwilkinsweb</SocialText>
+        <SocialIcon
+          color="white"
+          background={(props) => props.theme.green}
+          icon="linkedin"
+        />
+        <SocialText> /markwilkinsweb</SocialText>
       </SocialListItem>
     </SocialLit>
     <SideBarHeading> Profile </SideBarHeading>
-    <SideBarProfile> 
+    <SideBarProfile>
       <SidebarParagraph>
-        I am a Full-Stack Software Developer
-        skilled in developing dynamic data
-        driven applications. I work well
-        under pressure, and I consistently
-        deliver high quality work as efficiently
-        as possible.I am passionate about programming, learning, and building new things.
+        I am a Full-Stack Software Engineer skilled in developing dynamic data
+        driven applications. I work well under pressure, and I consistently
+        deliver high quality work as efficiently as possible. I am passionate
+        about programming, learning, and building new things.
       </SidebarParagraph>
     </SideBarProfile>
   </SideBar>
-)
+);
 
 export default ResumeSideBar;
